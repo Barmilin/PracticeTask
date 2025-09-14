ab47eaa9218063f707f7b4a152157b78df33e7ca
Barmilin(Vadim Ryazantsev) <little.troll.jr@gmail.com>
2025-09-14 21:15:20 +0300

    Add files via upload

---
+++ Task04/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        bankAccount acc = new bankAccount();
+
+        acc.owner = "John Wick";
+        acc.balance = 1000;
+
+
+    }
+}
